4c05c578a9a63a92ee97a9694e1d416b4a6befa1
Sylvia Alice <Sylviaalice58@gmail.com>
2026-02-04 17:57:36 +0300

    updated assignment

---
+++ assingment1.py
@@ -0,0 +1,15 @@
+# Assignment
+# 1.Write a python program that is able to calculate the BMI of a person whose weight is 78kg and height is 1.75meters. BMI=(weight)/(height squared)
+weight = 78
+height = 1.75
+BMI = (weight)/(height**2)
+print("This is the BMI", BMI)
+
+# 2.Find the are and perimeter of a rectangle whose length is 48 cm and width is 25 cm
+length = 48
+width = 25
+area =length * width
+perimeter = 2(length+width)
+print("this is the area", area)
+print("this is the perimeter", perimeter)
+#  Research on python list ,tuple and Dictionary Data types.
---
+++ lesson1d.py
@@ -0,0 +1,10 @@
+# create a python program that can able to calculate the simple interest given as the principle as 50000, rate as 5% and time as 8 years.SI =(p*R*T)/100
+principle = 50000
+time = 8
+rate = 5
+simple_interest = (principle*rate*time)/100
+print("The simple interest is:", simple_interest)
+
+
+
+
